89543ca2eb521072bc896798991db08be798b0e5
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2021-04-29 18:36:44 -0300

    fixed nlm.

--- code/apps/nlm.c
+++ code/apps/nlm.c
@@ -80,30 +80,35 @@ int main(int argc, char* argv[]) {
     // non-local means
     // search a window of size R
     //
-    const int R = 100;
-    const double h = 1.0;
+    const int R = 20;
+    const double h = 0.1;
     const int C = -0.5/(h*h);
     linear_template_t* ltpl = linearize_template(tpl,m,n);
     for (int i = 0, li = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j, ++li) {
             double y = 0; 
-	    double n = 0;
+	    double norm = 0;
+	    int count = 0;
             get_linear_patch(img,ltpl,i,j,NULL,pat);
             int di0 = i > R     ? i-R : 0;
             int di1 = i < (m-R) ? i+R : m;
             int dj0 = j > R     ? j-R : 0;
             int dj1 = j < (n-R) ? j+R : n;
+            //printf("di0 %d di1 %d dj0 %d dj1 %d\n",di0,di1,dj0,dj1);
             for ( int di = di0; di < di1; ++di ) {
                 for ( int dj = dj0; dj < dj1; ++dj ) {
                     get_linear_patch(img,ltpl,di,dj,NULL,pot);
 		    const double d = patch_dist(pat,pot,weights);
 		    const double w = exp(C*d);
 		    y += w*get_pixel(img,di,dj);
-                    n += w;
+                    norm += w;
+		    count ++;
                 }
             }
-	    const int x = (int) (0.5 + y/n);
+	    
+	    const int x = (int) (0.5 + y/norm);
             set_linear_pixel(&out,li,x);
+            //set_linear_pixel(&out,li,count/100);
 	}
     }
     //
