db17ceb97cdde18bfc7bf9601a005a1ea18a50d1
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2021-12-16 10:57:08 -0300

    oko yoko

--- scripts/test_sim.sh
+++ scripts/test_sim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 indir="data/luisa/clean"
 image="r0566_0019"
-template="n8star3"
+template="n8star4"
 perrsim=0.09
 # there is already some noise present and this is disastrous for DUDE-like rules if not
 # taken into account
@@ -11,7 +11,6 @@ outdir=results/${image}_p${perr}_${template}
 mkdir -p ${outdir}
 noisy=${outdir}/${image}_p${perr}.pbm
 #stats="results/luisa.stats"
-opt="--iterations=3"
 #
 # vanilla DUDE
 #
@@ -35,15 +34,30 @@ echo -e "\n\n===================================================================
 echo "VANILLA DUDE"
 output=${outdir}/${image}_p${perr}_dude_${template}.pnm
 dif=${outdir}/${image}_dude_${template}_p${perr}_dif.pnm
-echo build/apps/bin_dude --template=tpl/${template}.tpl --output=${output} --perr=${perr} ${opt} ${noisy}
 time build/apps/bin_dude --template=tpl/${template}.tpl --output=${output} --perr=${perr} ${opt} ${noisy}
 build/tools/compare --output=${dif} ${clean} ${output}
+
+echo -e "\n\n=========================================================================\n"
+echo "VANILLA DUDE x 3"
+output=${outdir}/${image}_p${perr}_dudex2_${template}.pnm
+dif=${outdir}/${image}_dudex2_${template}_p${perr}_dif.pnm
+time build/apps/bin_dude --template=tpl/${template}.tpl --output=${output} --perr=${perr} --iterations=3 ${noisy}
+build/tools/compare --output=${dif} ${clean} ${output}
+
 
 echo -e "\n\n=========================================================================\n"
 echo "QUORUM DUDE"
 output=${outdir}/${image}_p${perr}_quorum_${template}.pnm
 dif=${outdir}/${image}_quorum_${template}_p${perr}_dif.pnm
 time build/apps/quorum_den --template=tpl/${template}.tpl --output=${output} --perr=${perr} ${opt} ${noisy}
+echo -e "\n-----------------------------------------\n"
+build/tools/compare --output=${dif} ${clean} ${output}
+
+echo -e "\n\n=========================================================================\n"
+echo "QUORUM DUDE x 2"
+output=${outdir}/${image}_p${perr}_quorumx2_${template}.pnm
+dif=${outdir}/${image}_quorumx2_${template}_p${perr}_dif.pnm
+time build/apps/quorum_den --template=tpl/${template}.tpl --output=${output} --perr=${perr} --iterations=2 ${noisy}
 echo -e "\n-----------------------------------------\n"
 build/tools/compare --output=${dif} ${clean} ${output}
 
@@ -70,11 +84,11 @@ build/tools/compare --output=${dif} ${clean} ${output}
 #build/tools/compare --output=results/${image}_seminlm_${template}_p${perr}_dif.pnm ${clean} ${output}
 
 echo "VANILLA NLM"
-output=${outdir}/${image}_p${perr}_nlm_${template}.pnm
-dif=${outdir}/${image}_nlm_${template}_p${perr}_dif.pnm
-time build/apps/original_nlm --template=tpl/${template}.tpl --output=${output} --search=11 --perr=${perr} ${noisy}
-echo -e "\n-----------------------------------------\n"
-build/tools/compare --output=${dif} ${clean} ${output}
+#output=${outdir}/${image}_p${perr}_nlm_${template}.pnm
+#dif=${outdir}/${image}_nlm_${template}_p${perr}_dif.pnm
+#time build/apps/original_nlm --template=tpl/${template}.tpl --output=${output} --search=11 --perr=${perr} ${noisy}
+#echo -e "\n-----------------------------------------\n"
+#build/tools/compare --output=${dif} ${clean} ${output}
 
 #echo "VANILLA NLM"
 #build/apps/original_nlm --template=tpl/${template}.tpl --output=results/${image}_nlm_${template}_p${perr}.pnm ${noisy}
